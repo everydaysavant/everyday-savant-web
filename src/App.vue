bd896ea17415c689a93a7158b99c1d6332218f2a
Suraj Adhikari <suraj1.adhikari@ge.com>
2019-03-26 21:45:42 -0400

    Project related changes

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,25 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Everyday Savant</span>
+        <span class="font-weight-light">Development lab</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <HomePage/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HomePage from './components/HomePage'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage
   },
   data () {
     return {
---
+++ src/components/HomePage.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Everyday Savant Labs
+        </h1>
+        <p class="subheading font-weight-regular">
+          We meet your software development needs
+          
+        </p>
+      </v-flex>
+
+      <v-flex
+        mb-5
+        xs12
+      >
+        <h2 class="headline font-weight-bold mb-3">What do we do?</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      whatsNext: [
+        {
+          text: 'Artifical Intelligence',
+          href: 'https://vuetifyjs.com/components/api-explorer'
+        },
+        {
+          text: 'Image Processing',
+          href: 'https://vuetifyjs.com/layout/pre-defined'
+        },
+        {
+          text: 'App Development',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
+        }
+
+      ]
+    })
+  }
+</script>
+
+<style>
+
+</style>
